a9d88b9a7ea6ca8160527fb3733a563daadc496a
atrox3d <atrox3d@gmail.com>
2022-09-06 18:46:44 +0200

    106. Writing the Empty Classes

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask.views import MethodView
+from wtforms import Form
+
+
+class HomePage(MethodView):
+    pass
+
+
+class BillFormPage(MethodView):
+    pass
+
+
+class ResultsPage(MethodView):
+    pass
+
+
+class BillForm(Form):
+    pass
+

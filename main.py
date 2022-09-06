010a1cb2476313e52884876d5716e794b8b0e31b
atrox3d <atrox3d@gmail.com>
2022-09-06 19:33:25 +0200

    108. Implementing the Home Page

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from flask.views import MethodView
 from wtforms import Form
+from flask import Flask
+
+app = Flask(__name__)
 
 
 class HomePage(MethodView):
-    pass
+
+    def get(self):
+        return "Hello"
 
 
 class BillFormPage(MethodView):
-    pass
+    def get(self):
+        return "I am the bill form page"
 
 
 class ResultsPage(MethodView):
@@ -17,3 +23,8 @@ class ResultsPage(MethodView):
 class BillForm(Form):
     pass
 
+
+app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
+app.add_url_rule('/bill', view_func=BillFormPage.as_view('bill_form_page'))
+
+app.run()
